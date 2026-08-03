fd30de4a2d48fcd9e7395aecb8599e497e09615e
EA11R <EA11R@DESKTOP-8E1NMPC>
2026-08-03 10:36:48 +0900

    Update v1 5

--- js/controls.js
+++ js/controls.js
@@ -1,5 +1,56 @@
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+import {
+    state
+} from "./globals.js";
+
+
 export function initControls(){
 
-    console.log("controls");
+    console.log("initControls");
+
+
+    state.controls =
+        new OrbitControls(
+
+            state.camera,
+
+            state.renderer.domElement
+
+        );
+
+
+    state.controls.enableDamping =
+        true;
+
+
+    state.controls.target.set(
+
+        0,
+        0.5,
+        0
+
+    );
+
+
+    state.controls.minDistance =
+        2;
+
+
+    state.controls.maxDistance =
+        10;
+
+
+    state.controls.enablePan =
+        false;
+
+
+    state.controls.update();
+
+
+
+    console.log(
+        "controls initialized"
+    );
 
 }
